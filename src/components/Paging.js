43d374b988888b8d2ee80146c658709ea4c9ce93
Dmitrij <Dmitrijkamynin2@gmail.com>
2022-01-20 11:29:20 +0300

    add short page

--- src/components/Paging.js
+++ src/components/Paging.js
@@ -7,12 +7,23 @@ function Paging({tasks, checkPage, selectPage}) {
         <div className={styles.pagingconteiner}>
             <input className={styles.pagingcolStart} type="button" value={'<'} onClick={checkPage}></input>
                 {tasks.map((task, index) => {
-                    if ((index % 5) == 0) {
+                    if ((selectPage <= 5) && ((index % 5) == 0) && (index < 45)) {
                 return <PagingNumbers 
                         index={index / 5 + 1}
                         checkPage={checkPage}
                         selectPage={selectPage}/>
-                }})}
+                    } else if ((selectPage > ((tasks.length - 20) /5)) && ((index % 5) == 0) && (index >= (tasks.length - 45))) {
+                return <PagingNumbers 
+                        index={index / 5 + 1}
+                        checkPage={checkPage}
+                        selectPage={selectPage}/>
+                    } else if (((index % 5) == 0) && (index > selectPage * 5 - 30) && (index < selectPage * 5 + 20)) {
+                return <PagingNumbers 
+                        index={index / 5 + 1}
+                        checkPage={checkPage}
+                        selectPage={selectPage}/>        
+                    }
+                })}
             <input className={styles.pagingcolEnd} type="button" value={'>'} onClick={checkPage}></input>
         </div>
     )
