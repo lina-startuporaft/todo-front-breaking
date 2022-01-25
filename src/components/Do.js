ff5f33c6553ca9aae94b23d46f90465654510a6a
Dmitrij <Dmitrijkamynin2@gmail.com>
2022-01-25 19:04:30 +0300

    Add grid ffrom ant by tasks

--- src/components/Do.js
+++ src/components/Do.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styles from "../style/App.module.css"
 import 'antd/dist/antd.css'
-import { Checkbox, Button } from 'antd';
+import { Checkbox, Button, Row, Col } from 'antd';
 
 
 
@@ -36,40 +36,48 @@ function Do({task, delDo, checkboxChangeEdit, editTask}) {
     useEffect(() => {if (checkFocusTask == false) {refInput.current.focus()}},[checkFocusTask])
 
     return(
-        <div className={styles.do}>
-            <Checkbox
-                className={styles.coldoCol1}
-                onChange={checkboxChange}
-                name={task.id}
-                defaultChecked={task.checked}/>
-            {
-            checkFocusTask?
-                <p 
-                    className={styles.coldoCol2} 
-                    onMouseDown={focusTask}>
-                        {task.title}
-                </p>:
-                <input 
-                    ref={refInput} 
-                    onBlur={unFocusTask} 
-                    onKeyUp={chekEnterOrEsc} 
-                    className={styles.coldoCol2} 
-                    defaultValue={task.title} 
-                    id={task.id}
-                    maxLength="70">
-                    
-                </input>
-            }
-            <p className={styles.coldoCol3}>{task.date.slice(0, 10)}</p>
-            <Button
-                danger
-                className={styles.coldoCol4} 
-                value='del' 
-                onClick={delDo}
-                id={task.id}>
-                    Del
-            </Button>
-        </div>
+        <Row className={styles.do}>
+            <Col span={3}>
+                <Checkbox
+                    className={styles.colcol}
+                    onChange={checkboxChange}
+                    name={task.id}
+                    defaultChecked={task.checked}/>
+            </Col>
+            <Col span={15}>
+                {
+                checkFocusTask?
+                    <p 
+                        className={styles.colcol} 
+                        onMouseDown={focusTask}>
+                            {task.title}
+                    </p>:
+                    <input 
+                        ref={refInput} 
+                        onBlur={unFocusTask} 
+                        onKeyUp={chekEnterOrEsc} 
+                        className={styles.colcol} 
+                        defaultValue={task.title} 
+                        id={task.id}
+                        maxLength="70">
+                        
+                    </input>
+                }
+            </Col>
+            <Col span={3}>
+                <p className={styles.colcol}>{task.date.slice(0, 10)}</p>
+            </Col>
+            <Col span={3}>
+                <Button
+                    danger
+                    className={styles.colcol}
+                    value='del' 
+                    onClick={delDo}
+                    id={task.id}>
+                        Del
+                </Button>
+            </Col>
+        </Row>
     )
 }
 
