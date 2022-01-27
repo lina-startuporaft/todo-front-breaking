8eeb1a59a25f7e8d2c44dacf3983f24999253867
Dmitrij <Dmitrijkamynin2@gmail.com>
2022-01-27 13:30:32 +0300

    Upgrade animation edit tasks

--- src/components/Do.js
+++ src/components/Do.js
@@ -10,12 +10,14 @@ import '../style/Animation.css'
 function Do({task, delDo, editTaskGlobal}) {
 
     const [animation, setAnimation] = useState(false);
+    const [animationEdit, setAnimationEdit] = useState(false);
     const [titleTask, setTitleTask] = useState(task.title)
     const [currentFocus, setCurretFocus] = useState(false)
 
     useEffect(() => {
         setAnimation(!animation);
-    }, [])
+        setAnimationEdit(false);
+    }, [animationEdit])
 
     const focusTask = () => {
         setCurretFocus(true);
@@ -36,6 +38,7 @@ function Do({task, delDo, editTaskGlobal}) {
                     await editTaskGlobal(e.target.value, e.target.id, task.checked);
                     setTitleTask(e.target.value);
                     e.target.blur();
+                    setAnimationEdit(true);
                 } catch(err) {
                     if (err.message == 'Request failed with status code 400') {
                         message.error('there is already a task');
@@ -54,53 +57,56 @@ function Do({task, delDo, editTaskGlobal}) {
     const delDoTask = (e) => {
         e.currentTarget.disabled = 'true';
         delDo(e);
+        setAnimation(!animation);
     }
 
     return(
-        <CSSTransition in={animation} classNames="animation" timeout={300}>
-        <Row className={styles.do}>
-            <Col span={1}>
-            </Col>
-            <Col span={2}>
-                <Checkbox
-                    className={styles.colcoldocheck}
-                    onChange={checkboxChange}
-                    name={task.id}
-                    defaultChecked={task.checked}/>
-            </Col>
-            <Col span={15}>
-                    {
-                        currentFocus ?
-                            <Input
-                                defaultValue={titleTask}
-                                className={styles.coldotask}
-                                onBlur={unFocusTask}
-                                onKeyUp={chekEnterOrEsc}
-                                id={task.id}
-                                autoFocus>
-                            </Input> 
-                        :
-                            <p
-                                className={styles.coldotask} 
-                                onClick={focusTask}>
-                                    {titleTask}
-                            </p>
-                    }
-            </Col>
-            <Col span={3}>
-                <p className={styles.colcoldodate}>{task.date.slice(0, 10)}</p>
-            </Col>
-            <Col span={3}>
-                <Button
-                    danger
-                    className={styles.colcoldodel}
-                    value='del' 
-                    onClick={delDoTask}
-                    id={task.id}>
-                        Del
-                </Button>
-            </Col>
-        </Row>
+        <CSSTransition in={animation} classNames="animation" timeout={200}>
+            <CSSTransition in={animationEdit} classNames="animationEdit" timeout={200}>
+                <Row className={styles.do}>
+                    <Col span={1}>
+                    </Col>
+                    <Col span={2}>
+                        <Checkbox
+                            className={styles.colcoldocheck}
+                            onChange={checkboxChange}
+                            name={task.id}
+                            defaultChecked={task.checked}/>
+                    </Col>
+                    <Col span={15}>
+                            {
+                                currentFocus ?
+                                    <Input
+                                        defaultValue={titleTask}
+                                        className={styles.coldotask}
+                                        onBlur={unFocusTask}
+                                        onKeyUp={chekEnterOrEsc}
+                                        id={task.id}
+                                        autoFocus>
+                                    </Input> 
+                                :
+                                    <p
+                                        className={styles.coldotask} 
+                                        onClick={focusTask}>
+                                            {titleTask}
+                                    </p>
+                            }
+                    </Col>
+                    <Col span={3}>
+                        <p className={styles.colcoldodate}>{task.date.slice(0, 10)}</p>
+                    </Col>
+                    <Col span={3}>
+                        <Button
+                            danger
+                            className={styles.colcoldodel}
+                            value='del' 
+                            onClick={delDoTask}
+                            id={task.id}>
+                                Del
+                        </Button>
+                    </Col>
+                </Row>
+            </CSSTransition>
         </CSSTransition>
     )
 }
